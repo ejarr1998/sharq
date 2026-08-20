2d6be8395adf0b6c6708ba0ad0de740559287e06
ejarr1998 <ejarr1998@gmail.com>
2026-08-20 14:45:46 -0700

    Icon, manifest, SW cache bump for new theme

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -30,7 +30,7 @@ self.addEventListener('notificationclick', e => {
 });
 
 // Offline caching
-const CACHE = 'sharq-v1';
+const CACHE = 'sharq-v3';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', 'index.html', 'jobs.json', 'manifest.json'])));
   self.skipWaiting();
