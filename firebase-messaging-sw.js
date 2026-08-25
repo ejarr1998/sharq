099d491b35e993a1fca00a85e7c2980252782d07
ejarr1998 <ejarr1998@gmail.com>
2026-08-25 16:42:17 -0700

    Bump cache to sharq-v18 for account button restyle

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -23,7 +23,7 @@ self.addEventListener('notificationclick', e => {
 });
 
 // Offline caching
-const CACHE = 'sharq-v17';
+const CACHE = 'sharq-v18';
 const PRECACHE = ['./', 'index.html', 'jobs.json', 'manifest.json'];
 
 self.addEventListener('install', e => {
