685492b36cfaabb54dbcd71649441791ffb34979
ejarr1998 <ejarr1998@gmail.com>
2026-08-25 15:42:31 -0700

    Bump cache to sharq-v12 for auth+sync release

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -23,7 +23,7 @@ self.addEventListener('notificationclick', e => {
 });
 
 // Offline caching
-const CACHE = 'sharq-v11';
+const CACHE = 'sharq-v12';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', 'index.html', 'jobs.json', 'manifest.json'])));
   self.skipWaiting();
