9810960cb631840dd4cfa9a4b3e93f1cd954d83b
ejarr1998 <ejarr1998@gmail.com>
2026-08-27 08:10:28 -0700

    Bump cache to sharq-v24 for state-wipe fix

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -23,7 +23,7 @@ self.addEventListener('notificationclick', e => {
 });
 
 // Offline caching
-const CACHE = 'sharq-v23';
+const CACHE = 'sharq-v24';
 const PRECACHE = ['./', 'index.html', 'jobs.json', 'manifest.json'];
 
 self.addEventListener('install', e => {
