6bcd8362df1c9684622f1e35832b97c81fd3390a
ejarr1998 <ejarr1998@gmail.com>
2026-08-26 20:56:50 -0700

    Bump cache to sharq-v21 for load-race fix

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -23,7 +23,7 @@ self.addEventListener('notificationclick', e => {
 });
 
 // Offline caching
-const CACHE = 'sharq-v20';
+const CACHE = 'sharq-v21';
 const PRECACHE = ['./', 'index.html', 'jobs.json', 'manifest.json'];
 
 self.addEventListener('install', e => {
