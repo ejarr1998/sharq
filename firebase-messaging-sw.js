438702f4f79a4860f88011b8db21d88db7da063d
ejarr1998 <ejarr1998@gmail.com>
2026-08-27 08:56:09 -0700

    v27: cache bump

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -23,7 +23,7 @@ self.addEventListener('notificationclick', e => {
 });
 
 // Offline caching
-const CACHE = 'sharq-v25';
+const CACHE = 'sharq-v27';
 const PRECACHE = ['./', 'index.html', 'jobs.json', 'manifest.json'];
 
 self.addEventListener('install', e => {
